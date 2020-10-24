aeaabd87d41571b3061c7f6a8ba9995ca09e14ec
M.Kobeliev <kob85ua@gmail.com>
2020-10-24 17:26:38 +0300

    24.10.20

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import ContactList from "./components/ContactList";
+import InputBlock from "./components/InputBlock";
+import SearchForm from "./components/SearchForm";
+import {AppWrapper} from "./components/Styles"
+class App extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  changeFilter = (filter) => {
+    console.log(filter);
+    this.setState({ filter });
+  };
+
+  findContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter((contact) =>
+      contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+    );
+  };
+
+  addContact = (contactName, contactNumber) => {
+    const existContact = this.state.contacts.find(
+      ({ name }) => name === contactName
+    );
+    if (contactName==="" || contactNumber==="") {alert(`Fill in all fields`)}
+    else if (existContact !== undefined) {
+      alert(`${existContact.name} is already in contacts`);
+    } else {
+      const contact = {
+        name: contactName,
+        number: contactNumber,
+        id: uuidv4(),
+      };
+      this.setState((prevState) => {
+        return {
+          contacts: [...prevState.contacts, contact],
+        };
+      });
+    }
+  };
+  deleteContact = (contactId) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.findContacts();
+
+    return (
+      <AppWrapper>
+        <h2>Phonebook</h2>
+        <InputBlock onAddContact={this.addContact}></InputBlock>
+        <h2>Contacts</h2>
+        <SearchForm
+          value={filter}
+          onFindContacts={this.changeFilter}
+        ></SearchForm>
+        <ContactList
+          phoneContacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        ></ContactList>
+      </AppWrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/InputBlock.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import {InputBlockWrapper, Label, Input, Button} from "./Styles"
+class InputBlock extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  };
+  state = {
+    name: "",
+    number: "",
+  };
+  handleChange = ({ target }) => {
+    console.log(target.value);
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddContact(this.state.name, this.state.number);
+    this.setState({ name: "", number: "" });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <div>
+        <InputBlockWrapper onSubmit={this.handleSubmit}>
+          <Label>
+             <span>Name</span>
+            <Input
+              type="text"
+              
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </Label>
+          <Label>
+            <span>Number</span>
+            <Input
+              type="tel"
+              
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+            />
+          </Label>
+          <Button type="submit">Add contact</Button>
+        </InputBlockWrapper>
+      </div>
+    );
+  }
+}
+
+export default InputBlock;
